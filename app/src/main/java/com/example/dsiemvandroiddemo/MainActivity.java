21f059812b5fe340af835cdd844062a7846ba589
Jim Olsen <jim.olsen@dcap.com>
2026-07-21 17:19:50 -0400

    FIX: Responses from SureTap are intermittent because 2 instances of main activity exist.

--- app/src/main/java/com/example/dsiemvandroiddemo/MainActivity.java
+++ app/src/main/java/com/example/dsiemvandroiddemo/MainActivity.java
@@ -135,6 +135,9 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Bind the SDK to this Activity instance. Must happen in onCreate():
+        dsiEMVAndroidinstance.getInstance(this).setActivity(this);
+
         ViewPager2 viewPager = findViewById(R.id.optionPager);
         CardPagerAdapter adapter = new CardPagerAdapter(this);
         viewPager.setAdapter(adapter);
@@ -510,7 +513,11 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     protected void onDestroy() {
-        dsiEMVAndroidinstance.getInstance(MainActivity.this).clearActivity();
+        // Only clear when destroyed for good.
+        if (isFinishing())
+        {
+            dsiEMVAndroidinstance.getInstance(this).clearActivity();
+        }
         super.onDestroy();
     }
     public void onRadioButtonClicked(View view)
